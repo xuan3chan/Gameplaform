2a5f27d6e9f70f8e704c287354e0fc57c442f68c
Trần Nguyễn Tú Nhi <tunhi1042@gmail.com>
2023-07-08 18:14:58 +0700

    create class Element

---
+++ app/src/main/java/teammeme/code/Element.java
@@ -0,0 +1,30 @@
+package teammeme.code;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+
+public class Element {
+    Bitmap bitmap; //hinh anh
+    int mX; //toa do x
+    int mY; //toa do y
+
+    public Element(Resources res, int x, int y)
+    {
+        bitmap= BitmapFactory.decodeResource(res,R.drawable.hinhre);
+        mX=x-bitmap.getWidth()/2;
+        mY=y-bitmap.getHeight()/2;
+    }
+    public Element(Resources res, int x, int y, int idHinh)
+    {
+        bitmap=BitmapFactory.decodeResource(res,idHinh);
+        mX=x-bitmap.getWidth()/2;
+        mY=y-bitmap.getHeight()/2;
+    }
+    public void doDraw(Canvas canvas)
+    {
+        canvas.drawBitmap(bitmap, mX,mY, null);
+    }
+
+}
