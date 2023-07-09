abad21f998f7c6f27d2a7f1f48e63c0c51a32c8d
xuandd <103272537+xuanchimto@users.noreply.github.com>
2023-07-09 13:48:43 +0700

    hinh nen backgroud

--- app/src/main/java/teammeme/code/ParallaxBackground.java
+++ app/src/main/java/teammeme/code/ParallaxBackground.java
@@ -8,50 +8,44 @@ import android.graphics.Canvas;
 public class ParallaxBackground {
     private int toadonen1_X = 0;
     private int toadonen2_X = 0;
+    private int toadonen3_X = 0;
     private Bitmap hinhnen1;
     private Bitmap hinhnen2;
+    private Bitmap hinhnen3;
 
-    public ParallaxBackground(Resources c)
-    {
-        hinhnen1 = BitmapFactory.decodeResource(c, R.drawable.khonggian);
-        hinhnen2 = BitmapFactory.decodeResource(c, R.drawable.hanhtinh);
-        //hinhnen3 = BitmapFactory.decodeResource(c, R.drawable.nui);
+    public ParallaxBackground(Resources resources) {
+        hinhnen1 = BitmapFactory.decodeResource(resources, R.drawable.may);
+        hinhnen2 = BitmapFactory.decodeResource(resources, R.drawable.nui);
+        hinhnen3 = BitmapFactory.decodeResource(resources, R.drawable.dat);
     }
+
     public void doDrawRunning(Canvas canvas) {
-
-        //giam toa do de dich chuyen cho nen1
-        toadonen1_X = toadonen1_X - 1;
-
-        //giam toa do de dich chuyen cho nen2
-        toadonen2_X = toadonen2_X - 4;
-
-        // tinh do lech cho hinh 2 (xem hinh minh hoa)
-        int toadonen1_phu_X = hinhnen1.getWidth() - (-toadonen1_X);
-
-        //da di chuyen het thi quay lai tu dau
-        if (toadonen1_phu_X <= 0) {
+        // Update X-coordinate for background 1
+        toadonen1_X -= 1;
+        if (toadonen1_X <= -hinhnen1.getWidth()) {
             toadonen1_X = 0;
-            // chi can ve 1 tam
-            canvas.drawBitmap(hinhnen1,0, 0, null);
-
-        } else {
-            // ve 1 tam lech va tam 2 noi duoi theo
-            canvas.drawBitmap(hinhnen1, toadonen1_X, 0, null);
-            canvas.drawBitmap(hinhnen1, toadonen1_phu_X, 0, null);
         }
 
-        int toadonen2_phu_X = hinhnen2.getWidth() - (-toadonen2_X);
-
-        if (toadonen2_phu_X <= 0) {
+        // Update X-coordinate for background 2
+        toadonen2_X -= 4;
+        if (toadonen2_X <= -hinhnen2.getWidth()) {
             toadonen2_X = 0;
-            canvas.drawBitmap(hinhnen2, toadonen2_X, 0, null);
-
-        } else {
-            canvas.drawBitmap(hinhnen2, toadonen2_X, 0, null);
-            canvas.drawBitmap(hinhnen2, toadonen2_phu_X, 0, null);
         }
 
+        // Update X-coordinate for background 3
+        toadonen3_X -= 9;
+        if (toadonen3_X <= -hinhnen3.getWidth()) {
+            toadonen3_X = 0;
+        }
+
+        // Draw the backgrounds
+        canvas.drawBitmap(hinhnen1, toadonen1_X, 0, null);
+        canvas.drawBitmap(hinhnen1, toadonen1_X + hinhnen1.getWidth(), 0, null);
+
+        canvas.drawBitmap(hinhnen2, toadonen2_X, 0, null);
+        canvas.drawBitmap(hinhnen2, toadonen2_X + hinhnen2.getWidth(), 0, null);
+
+        canvas.drawBitmap(hinhnen3, toadonen3_X, 4, null);
+        canvas.drawBitmap(hinhnen3, toadonen3_X + hinhnen3.getWidth(), 0, null);
     }
-
-
 }
